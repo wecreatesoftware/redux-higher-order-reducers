5267dd35079a5ee00d53df8c9feb793c68e4a088
mstruensee <mstruensee@gmail.com>
2019-03-08 19:55:10 -0600

    cleanup

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,7 +5,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /(node_modules)/,
-                loader: "babel-loader"
+                loader: "babel-loader",
             },
         ],
     },
