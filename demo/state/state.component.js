7758b7bd7a26e788872fae4e1d923a848fb8a195
mstruensee <mstruensee@gmail.com>
2019-05-30 20:44:23 -0500

    removed demo

--- demo/state/state.component.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import PropTypes from "prop-types"
-
-export const StateComponent = ({ state }) => <pre>{ JSON.stringify(state, undefined, 2) }</pre>
-
-StateComponent.propTypes = {
-    state: PropTypes.shape().isRequired,
-}
--- demo/state/state.container.js
+++
@@ -1,9 +0,0 @@
-import { connect } from "react-redux"
-import { StateComponent } from "./state.component"
-
-const mapStateToProps = state => ({ state })
-
-export const StateContainer = connect(
-    mapStateToProps,
-    null,
-)(StateComponent)
